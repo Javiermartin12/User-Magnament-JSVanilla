80e7dce4998f172b9c0cc883d5a46f39058273d7
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-07-08 12:59:23 +0200

    Chore: database created with the method POST to form user

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-document.addEventListener('DOMContentLoaded', () =>{
 
+document.addEventListener('DOMContentLoaded', () =>{
+  document.getElementById('createUser').addEventListener('submit', async function(event){
+    event.preventDefault()
+
+    const id = document.getElementById('idUser').value
+    const name = document.getElementById('userName').value
+    const email = document.getElementById('inputEmail').value
+    const password = document.getElementById('inputPassword').value
+    const data = { id, name, email, password}
+    console.log(data)
+    try {
+        const response = await fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+            
+        })
+        if (response.ok) {
+            this.classList.add('hidden')
+        }
+        alert('User saved correctly');
+    } catch (error) {
+        console.error('Error:', error);
+        alert('There was a problem to signup user');
+    }
+  })
 })
 
