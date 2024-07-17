bad85e8027a98f1363c3e3b6ad9c2c1b8b89a365
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-07-17 13:32:46 +0200

    Chore: changes

--- JS/app.js
+++ JS/app.js
@@ -4,14 +4,14 @@ import { postUser } from "./services/userServices/postUser.js"
 import { getFriends } from "./services/userServices/getFriends.js"
 import { postTraining } from "./services/trainingServices/postTraining.js"
 import { getTrainingTSheets } from "./services/trainingServices/getTraningTSheet.js"
-import { updateUser } from "./services/userServices/updateUser.js"
+// import { updateUser } from "./services/userServices/updateUser.js"
 // import { putUserUpdated } from "./services/userServices/updateUser.js"
 document.addEventListener('DOMContentLoaded', () =>{
     pagination()
     postUser()
     getUsers()
     getFriends()
-    updateUser()
+    // updateUser()
     // putUserUpdated()
 
     postTraining()
