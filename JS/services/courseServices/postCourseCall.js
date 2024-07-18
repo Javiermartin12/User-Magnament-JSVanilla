7711f874649453d7311816d7ce76ff35124764fc
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-07-18 12:19:43 +0200

    Chore: catched value of form in post file

--- JS/services/courseServices/postCourseCall.js
+++ JS/services/courseServices/postCourseCall.js
@@ -3,4 +3,19 @@ import { getUnitServiceToPopulate } from "./dinamicForm/getUnitServiceToPopulate
 export const postCourseCall = () => {
     getTrainingNameToPopulate()
     getUnitServiceToPopulate()
+
+    const submitCourseCall = document.getElementById('submitCourseCall')
+    submitCourseCall.addEventListener('click', async (event) =>{
+        event.preventDefault()
+        const convocationTrainingName = document.getElementById('convocationTrainingName').value
+        const convocationUnitService = document.getElementById('convocationUnitService').value
+        const startDate = document.getElementById('startDate').value
+        const endDate = document.getElementById('endDate').value
+        const communicateDaysBefore = document.getElementById('communicateDaysBefore').value
+        const timetable = document.getElementById('timetable').value
+        const convocationTheoreticalInstructor = document.getElementById('convocationTheoreticalInstructor').value
+        const convocationInstructors = document.getElementById('convocationInstructors').value
+        const convocationAssessor = document.getElementById('convocationAssessor').value
+
+    })
 }
