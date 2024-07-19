0c017d3a34e3a3ce3a8dc1f7ee3ea997a8a407a4
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-07-19 11:07:01 +0200

    Chore: delete consolelog

--- JS/services/courseServices/dinamicForm/sendConvocation.js
+++ JS/services/courseServices/dinamicForm/sendConvocation.js
@@ -2,34 +2,33 @@ export const sendConvocation = () => {
     try {
          const starDateInput = document.getElementById('startDate')
          const startDate = new Date(starDateInput.value)
-            console.log(startDate)
+
 
          const communicateDaysBeforeInput = document.getElementById('communicateDaysBefore')
          const communicateDaysBefore = parseInt(communicateDaysBeforeInput.value)
-            console.log(communicateDaysBefore)
+
 
          //Calculate the limit time to send convocation
          const comunicationDate = new Date(startDate)
          comunicationDate.setDate(startDate.getDate() - communicateDaysBefore)
-             console.log(comunicationDate)
+
 
         const today = new Date()
-         console.log(today)
+
     
-         const sendCommunication = () => {
-        console.log('Sending convocation...');
-
-         }
-         if(today > comunicationDate) {
+        if(today > comunicationDate) {
             sendCommunication()
-             console.log('Convocation sended automaticly')
+            console.log('Convocation sended automaticly')
         }else {
-        console.log('It is not time to send the call')
-         }
-         } catch (error){
-             console.error('Error to send the convocation:', error);
-             }
-
-
+            console.log('It is not time to send the call')
+        }
+    } catch (error){
+        console.error('Error to send the convocation:', error);
+    }
+    
+    
+}
+const sendCommunication = () => {
+    console.log('Sending convocation...');
 }
 sendConvocation()
