f0774394c47c7c4494bee2d5a733cdb2d125532f
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-07-19 14:17:56 +0200

    Chore: created post from courseCall to data base

--- JS/services/courseServices/postCourseCall.js
+++ JS/services/courseServices/postCourseCall.js
@@ -1,22 +1,73 @@
 import { getTrainingNameToPopulate } from "./dinamicForm/getTrainingNameToPopulate.js"
 
 import { sendConvocation } from "./dinamicForm/sendConvocation.js"
-export const postCourseCall = () => {
+export const postCourseCall =  () => {
     getTrainingNameToPopulate()
+    const createCourse = document.getElementById('createCourse')
+    if (!createCourse) {
+        console.error('Form not found');
+        return;
+    }
 
-
-    const submitCourseCall = document.getElementById('submitCourseCall')
-    submitCourseCall.addEventListener('click', async (event) =>{
+    const submitCourseCall = document.getElementById('createCourse')
+    submitCourseCall.addEventListener('submit', async (event) =>{
         event.preventDefault()
         const convocationTrainingName = document.getElementById('convocationTrainingName').value
-        const convocationUnitService = document.getElementById('convocationUnitService').value
+        console.log(convocationTrainingName)
+        const convocationUnitService = Array.from(document.getElementById('convocationUnitService').selectedOptions).map(option => option.textContent)
+        console.log(convocationUnitService)
         const startDate = document.getElementById('startDate').value
+        console.log(startDate)
         const endDate = document.getElementById('endDate').value
+        console.log(endDate)
         const communicateDaysBefore = document.getElementById('communicateDaysBefore').value
+        console.log(communicateDaysBefore)
         const timetable = document.getElementById('timetable').value
-        const convocationTheoreticalInstructor = document.getElementById('convocationTheoreticalInstructor').value
-        const convocationInstructors = document.getElementById('convocationInstructors').value
-        const convocationAssessor = document.getElementById('convocationAssessor').value
+        console.log(timetable)
+        const convocationTheoreticalInstructor = Array.from(document.getElementById('convocationTheoreticalInstructor').selectedOptions).map(option => option.textContent)
+        console.log(convocationTheoreticalInstructor)
+        const convocationInstructors = Array.from(document.getElementById('convocationInstructors').selectedOptions).map(option => option.textContent)
+        console.log(convocationInstructors)
+        const convocationAssessor = Array.from(document.getElementById('convocationAssessor').selectedOptions).map(option => option.textContent)
+        console.log(convocationAssessor)
         sendConvocation()
+
+        if (!convocationTrainingName || !convocationUnitService.length || !startDate || !endDate || !communicateDaysBefore || !timetable
+            || !convocationTheoreticalInstructor.length || !convocationInstructors.length || !convocationAssessor.length) {
+            alert('Complete the form')
+            return
+        }
+
+        const data = {convocationTrainingName,
+             convocationUnitService,
+            startDate,
+            endDate,
+            communicateDaysBefore,
+            timetable,
+            convocationTheoreticalInstructor,
+            convocationInstructors,
+           convocationAssessor
+            }
+            console.log(data)
+            try {
+                const response = await fetch('http://localhost:3000/courseCall', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body:JSON.stringify(data)
+                    
+                })
+                if (response.ok) {
+                    alert('CourseCall saved correctly');
+                } else {
+                    throw new Error('Failed to save CourseCall');
+                }
+                console.log(response)
+                alert('CourseCall saved correctly')
+            } catch (error) {
+                alert('There was a problem to post courseCall');
+                console.log(error)
+            }
     })
 }
