9eb35785e2f1b9ccf218d2980bd487421f8ec0c0
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-07-20 22:42:55 +0200

    Chore: created funcionality of delete courses

---
+++ JS/services/courseServices/deleteCourseCall.js
@@ -0,0 +1,14 @@
+export const deleteCourseCall = async (id) => {
+
+    try {
+        const response = await fetch(`http://localhost:3000/courseCall/${id}`, {
+            method: 'DELETE'
+        })
+        if (!response.ok) {
+            console.log('Error TO DELETE user');
+        }
+    } catch (error) {
+        console.log('User not deleted')
+    }
+
+}
