7f7fd0c6b4269b09b4ab576a7cefeae4119294e3
javiermartinruiz <javiermartinruiz21@gmail.com>
2024-07-22 10:26:48 +0200

    Chore: cleaned clg in this file

--- JS/services/courseServices/getCourseCall.js
+++ JS/services/courseServices/getCourseCall.js
@@ -2,15 +2,13 @@ import { deleteCourseCall } from "./deleteCourseCall.js"
 export const getCourseCall = async () => {
     try {
         const response = await fetch('http://localhost:3000/courseCall')
-        console.log(response)
         const courseCalls = await response.json()
         if (!Array.isArray(courseCalls) || courseCalls.length === 0) {
             console.error('There are no courseCall')
             alert('There are no courseCall')
             return
         }
-        console.log(courseCalls)
-         
+        
         const courseCallTable = document.querySelector('#courseCallTable tbody')
         courseCalls.forEach(courseCall => {
 
